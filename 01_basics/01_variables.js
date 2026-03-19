f1c37d36c57c4f3c8aa78fd5e8e20753f0da4c23
abhishekumar331 <abhishekumar331@gmail.com>
2026-03-19 17:23:55 +0530

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "abhishek@gmail.com"
+var accountPassword = "123@#"
+accountCity = "Bangalore"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "there@gmail.com"
+accountPassword = "1123@#"
+accountCity = "delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])

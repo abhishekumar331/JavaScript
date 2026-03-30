c96350a27222737341c05ad361a87511f0ccf5e7
Abhishek kumar <abhishekumar331@gmail.com>
2026-03-30 21:30:49 +0530

    Add files via upload

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"
+
+console.log(3 + 3)
+
+console.log("abhishek")
+
+let name = "abhishek"
+let age = 19
+let isLoggedIn = false
+let state; 
+
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "abhi"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+
+let isLoggedIn = "abhishek"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn)
+
+let someNumber = 34
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+// ****** operations ******
+
